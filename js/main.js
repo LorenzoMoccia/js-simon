e1a640f185ed676d70642a037d9f04184879c149
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-05 21:25:32 +0100

    Function generateRandomNumber

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+
+
+
+
+
+function generateRandomNumber(){
+
+    const randomArray = [];
+
+    while(randomArray.length < 5){
+
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    if(randomArray.indexOf(randomNumber) === -1) randomArray.push(randomNumber);
+
+    }
+
+    console.log(randomArray);
+
+}
