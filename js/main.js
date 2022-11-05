1fbc5275a734de3bf3af1a90252795bdf720c0fe
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-05 22:04:47 +0100

    Terminato

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 "use strict";
-
+let randomNumberArray = [];
 //Genero un Array con 5 numeri random tra 1 e 100, diversi tra loro.
 generateRandomNumber();
 //Nascondo i numeri
 hideNumber();
 //Chiedo all'utente i numeri da indovinare dopo 40 secondi
-setTimeout(askForNumber, 4000);
+setTimeout(askForNumberAndCheck, 31000);
 
 
 
@@ -17,17 +17,17 @@ setTimeout(askForNumber, 4000);
 -------------------------------------------------*/
 function generateRandomNumber(){
 
-    const randomArray = [];
 
-    while(randomArray.length < 5){
+
+    while(randomNumberArray.length < 5){
 
     let randomNumber = Math.floor(Math.random() * 100) + 1;
-    if(randomArray.indexOf(randomNumber) === -1) randomArray.push(randomNumber);
+    if(randomNumberArray.indexOf(randomNumber) === -1) randomNumberArray.push(randomNumber);
 
     }
 
-    document.getElementById("random-number").innerHTML = randomArray;
-    console.log(randomArray);
+    document.getElementById("random-number").innerHTML = randomNumberArray;
+    
 
 }
 
@@ -38,17 +38,32 @@ function hideNumber(){
 
         document.getElementById("random-number").innerHTML = "";
 
-    }, 3000)
+    }, 30000)
 }
 
-function askForNumber(){
+function askForNumberAndCheck(){
 
     let numberArray = [];
+    let correctNumberArray = [];
 
     for(let i = 0; i < 5; i++){
 
         numberArray[i] = Number(prompt("Inserisci un numero:"));
+
+        if(randomNumberArray.includes(numberArray[i])){
+            correctNumberArray.push(numberArray[i]);
+        }
     }
 
+    document.getElementById("correct-number").innerHTML = "I numeri che hai indovinato: " + correctNumberArray;
+    document.getElementById("total-correct-number").innerHTML = "Quanti numeri hai indovinato: " + correctNumberArray.length;
+
+
     console.log(numberArray);
+    console.log(randomNumberArray);
+    console.log(correctNumberArray);
+
+    
+
+    
 }
