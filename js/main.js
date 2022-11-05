93d0795d20655ef957127b89a3edacadf33b4eef
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-05 21:32:40 +0100

    function hideNumber

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 "use strict";
 
+//Genero un Array con 5 numeri random tra 1 e 100, diversi tra loro.
+generateRandomNumber();
+//Nascondo i numeri
+hideNumber();
 
 
 
 
 
+
+
+/*-----------------------------------------------
+            FUNCTIONS
+-------------------------------------------------*/
 function generateRandomNumber(){
 
     const randomArray = [];
@@ -16,6 +25,17 @@ function generateRandomNumber(){
 
     }
 
+    document.getElementById("random-number").innerHTML = randomArray;
     console.log(randomArray);
 
+}
+
+
+function hideNumber(){
+
+    setTimeout(function(){
+
+        document.getElementById("random-number").innerHTML = "";
+
+    }, 3000)
 }
